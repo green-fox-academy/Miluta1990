9b735dd2d805cc92a40f0802d6fac21904cdeccb
Milan Bartha <bartha.milan@gmail.hu>
2019-12-05 16:41:06 +0100

    finished diamond

---
+++ week-01/day-4/diamond.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+let lineCount: number = 7;
+
+// Write a program that draws a
+// diamond like this:
+//
+//    *      1 csillag 3-3 szóköz
+//   ***     3 csillag 2-2 szóköz
+//  *****   5 csillag 1-1 szóköz
+// *******   7 csillag 0 szóköz
+//  ***** 
+//   ***
+//    *
+//
+// The diamond should have as many lines as lineCount i
+
+
+console.log('   *   ')
+console.log('  ***   ')
+console.log(' ***** ')
+console.log('******* ')
+console.log(' ***** ')
+console.log('  ***  ')
+console.log('   *  ')
