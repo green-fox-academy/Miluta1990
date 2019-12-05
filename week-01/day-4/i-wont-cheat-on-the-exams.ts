73974b2cef757532435d4aaa8d9086bef534992d
Milan Bartha <bartha.milan@gmail.hu>
2019-12-05 14:51:53 +0100

    finished wont cheat on exams

---
+++ week-01/day-4/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+let a: number = 0
+let iWont: string = 'I won\'t cheat on the exam!'
+
+while (a <=100) {console.log(iWont);
+a++; }
