47ef2e44be1e8bd02038704dfa2c3bd851208373
Milan Bartha <bartha.milan@gmail.hu>
2019-12-06 10:37:05 +0100

    finished pyramid, modified triangle

--- week-01/day-4/pyramid.ts
+++ week-01/day-4/pyramid.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-let lineCount: number = 4;
+
 
 // Write a program that draws a
 // pyramid like this:
@@ -12,12 +12,19 @@ let lineCount: number = 4;
 //
 // The pyramid should have as many lines as lineCount is
 
+let lineCount: number = 5;
+let star: string = '*';
+let out: string = '';
 
-function generatePyramid(num) {
-    let number = '';
-  
-    for (let i = 1; i <= num; i++) {
-      console.log(number += i);
-    }
-  }
-  generatePyramid(5)
+for (let i: number = 0; i < lineCount; i++) {
+    out = '';
+    for (let j: number = 0; j < lineCount - i; j++) {
+        out = out + ' '; 
+    } 
+    for (let k: number = 1; k <= 2 * i + 1; k++) {
+            out = out + star; 
+        }
+    
+   
+        console.log(out);
+}
--- week-01/day-4/triangle.ts
+++ week-01/day-4/triangle.ts
@@ -11,11 +11,12 @@ let lineCount: number = 4;
 //
 // The triangle should have as many lines as lineCount is
 
-let bodyTriangle: string = '';
-let i:number=1;
-
-while (i <= lineCount) {
-    bodyTriangle = bodyTriangle + '*';
-    console.log(bodyTriangle);
-    i++;
+let star: string = '*';
+let out: string = '';
+for (let i: number = 0; i < lineCount; i++) {
+    out = '';
+    for (let j: number = 0; j < i + 1; j++) {
+        out = out + star;
+    }
+    console.log(out);
 }
