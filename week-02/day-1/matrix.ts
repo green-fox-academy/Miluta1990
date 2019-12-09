97fcacf20a8b99def068e8dc905fe07dfd9b44cc
Milan Bartha <bartha.milan@gmail.hu>
2019-12-09 17:02:05 +0100

    playing with matrix

--- week-02/day-1/matrix.ts
+++ week-02/day-1/matrix.ts
@@ -13,14 +13,20 @@
 // * size should depend on a variable
 // ** Relax, a matrix is just like an array
 
-let myMatrix: number[][] = [[0,0,0,1], [0,0,1,0],[0,1,0,0], [1,0,0,0]]; 
-
-for(let i:number; i < myMatrix.length; i++) {
-    for (let j:number = 0; j < myMatrix[i].length, i++;);
-   
-    console.log(myMatrix[1]);      
-
+function matrix(lengthOfRow) {
+    let x: number[][] = [];
+    
+    for (let outerArray: number = 0; outerArray < lengthOfRow; outerArray++) {
+        let row: number[] = [];
+        for (let column: number = 0; column < lengthOfRow - outerArray - 1; column++) {
+            row.push(0);
+        }
+        row.push(1);
+        for (let columnsAfterOne: number = 0; columnsAfterOne < outerArray; columnsAfterOne++) {
+            row.push(0);
+        }
+        x.push(row);
+    }
+    console.log(x);
 }
-
-
-
+matrix(5);
