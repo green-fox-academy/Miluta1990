8d1792d6a79827861771bf88d7a4de0e180db62d
Milan Bartha <bartha.milan@gmail.hu>
2019-12-09 13:39:20 +0100

    sum and swap elements done

---
+++ week-02/day-1/sum-element.ts
@@ -0,0 +1,14 @@
+'use strict'
+
+// -  Create a variable named `r` with the following content: `[54, 23, 66, 12]`
+// -  Print the sum of the second and the third element
+
+let r: number[]= [54,23,66,12];
+
+function sumR(sumItems) {
+
+    return sumItems[2] + sumItems[3];
+}
+
+console.log(sumR(r));
+
---
+++ week-02/day-1/swap-elements.ts
@@ -0,0 +1,14 @@
+'use strict'
+
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
+
+let abc: string[]= ['Arthur', 'Boe', 'Chloe'];
+
+abc[3] = abc[0];
+abc[0] = abc[2]
+
+abc.splice(2,1);
+
+
+console.log(abc);
