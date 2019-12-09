50c19b1ea145a80da01d1dbb148e7dcca52c83f2
Milan Bartha <bartha.milan@gmail.hu>
2019-12-09 14:57:56 +0100

    done doubling, gave up on matrix for now

---
+++ week-02/day-1/matrix.ts
@@ -0,0 +1,26 @@
+'use strict'
+
+// -  Create (dynamically*) a two dimensional list
+//    with the following matrix**. Use a loop!
+//
+//    0 0 0 1
+//    0 0 1 0
+//    0 1 0 0
+//    1 0 0 0
+//
+// -  Print this two dimensional list to the console
+//
+// * size should depend on a variable
+// ** Relax, a matrix is just like an array
+
+let myMatrix: number[][] = [[0,0,0,1], [0,0,1,0],[0,1,0,0], [1,0,0,0]]; 
+
+for(let i:number; i < myMatrix.length; i++) {
+    for (let j:number = 0; j < myMatrix[i].length, i++;);
+   
+    console.log(myMatrix[1]);      
+
+}
+
+
+
