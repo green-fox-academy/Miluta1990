c8be78fb108de9770535c0da434b9f157c96b390
Milan Bartha <bartha.milan@gmail.hu>
2019-12-12 16:15:29 +0100

    finished square positioning

---
+++ week-02/day-4/square-positioning.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Create a function that draws one square and takes 2 parameters:
+// The x and y coordinates of the square's top left corner
+// and draws a 50x50 square from that point.
+// Draw 3 squares with that function.
+// Avoid code duplication.
+
+function drawSquare(topX, topY) {
+
+    ctx.strokeRect(topX, topY, 50, 50);
+
+}
+for (let i: number =0; i <= 600; i+=150) {
+
+drawSquare (0, 0+i);
+}
+
+
+
