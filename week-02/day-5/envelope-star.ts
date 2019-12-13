05fd492b1988a480f5d981ff1833e022f3ab0f7c
Milan Bartha <bartha.milan@gmail.hu>
2019-12-13 13:13:11 +0100

    done envelope star yesssssssssss

---
+++ week-02/day-5/envelope-star.ts
@@ -0,0 +1,23 @@
+'use strict'
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+
+// width = 400 length = 400;
+
+let stepper: number = 20; 
+ctx.translate(canvas.width/2, canvas.height/2)
+for (let j:number = 0 ; j <= 4 ; j++) {
+
+    ctx.rotate(90*Math.PI / 180);
+
+
+for (let i:number = 0; i<=canvas.width/stepper/2 ; i++) {
+
+    ctx.beginPath();
+    ctx.moveTo((i+1)*stepper , 0);
+    ctx.strokeStyle = 'orange';
+    ctx.lineTo(0, canvas.height /2 -((i-1)*stepper  ));
+    ctx.stroke()
+}}
