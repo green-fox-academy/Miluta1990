382867bf12f3ff08c659a2359d0764605a2730e4
Milan Bartha <bartha.milan@gmail.hu>
2020-01-08 11:57:31 +0100

    finished instruments test 1

--- week-04/day3/instruments.ts
+++ week-04/day3/instruments.ts
@@ -16,19 +16,59 @@ Bass Guitar (4 strings, 'Duum-duum-duum')
 Violin (4 strings, 'Screech')
 The Workshop should be invoked from another file like app.ts or main.ts*/
 
-abstract class Instrument {
+export abstract class Instrument {
     protected name: string;
   
     constructor(name){
         this.name = name;
     }
     play(){};
-}
-abstract class StringedInstrument extends Instrument {
+};
+export abstract class StringedInstrument extends Instrument {
     numberOfStrings: number;
     constructor(name: string, stringNumber: number){
         super(name);
         this.numberOfStrings = stringNumber;
     }
     sound(){};
-}
+};
+
+export class ElectricGuitar extends StringedInstrument {
+    constructor(stringNumber: number = 6) {
+    super('Electric Guitar', stringNumber);
+    }
+
+    sound() {
+        return 'Twang';
+    }
+    play() {
+        console.log(`${this.name} a ${this.numberOfStrings}-stringed instrument that goes ${this.sound()}`)
+    }
+};
+
+export class BassGuitar extends StringedInstrument {
+    constructor(stringNumber: number = 4) {
+    super('Bass Guitar', stringNumber);
+    }
+
+    sound() {
+        return 'Duum-duum-duum';
+    }
+    play() {
+        console.log(`${this.name} a ${this.numberOfStrings}-stringed instrument that goes ${this.sound()}`)
+    }
+};
+
+export class Violin extends StringedInstrument {
+    constructor(stringNumber: number = 4) {
+    super('Violin', stringNumber);
+    }
+
+    sound() {
+        return 'Screech';
+    }
+    play() {
+        console.log(`${this.name} a ${this.numberOfStrings}-stringed instrument that goes ${this.sound()}`)
+    }
+};
+
