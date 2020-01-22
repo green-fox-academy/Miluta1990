6fed799769090e0f8e181b5df33e97e977513d36
Milan Bartha <bartha.milan@gmail.hu>
2020-01-22 14:54:00 +0100

    done container 1

---
+++ week-06/day-03/content-1.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+let pList = document.getElementsByTagName('p')
+
+for(let i = 0;i<pList.length;i++){
+  pList[i].innerText = 'goat rabbit cat dog'
+}
+
+
+// this sets every <p> tag to strong cat
+
+
+let pList2 = document.getElementsByTagName('p')
+
+for(let i = 0;i<pList2.length;i++){
+  pList2[i].innerHTML = pList2[pList2.length-1].innerHTML
+}
