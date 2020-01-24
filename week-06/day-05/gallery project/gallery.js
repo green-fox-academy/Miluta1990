4a4333c75d9c6efde958d6b75f138177e07e8a57
Milan Bartha <bartha.milan@gmail.hu>
2020-01-24 09:56:06 +0100

    created project folder, downloaded pics and started the html code

---
+++ week-06/day-05/gallery project/gallery.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const listOfImages = [
+  {name:'pic1.jpg', title:'Goku & Vegeta', story:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure numquam modi voluptatem nam. Reprehenderit eos, aperiam optio dolorem eligendi nobis molestiae magnam placeat sit, architecto mollitia, ullam accusantium consequuntur'},
+  {name:'pic2.jpg', title:'Ultra Insict Goku', story:'Distinctio iure numquam modi voluptatem nam. Reprehenderit eos, aperiam optio dolorem eligendi nobis molestiae magnam placeat sit, architecto mollitia, ullam accusantium consequuntur.'},
+  {name:'pic3.jpg', title:'Ascended Trrunks', story:'Ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure numquam modi voluptatem nam ullam accusantium consequuntur.'},
+  {name:'pic4.jpg', title:'Blue Vegeta', story:'Adipisicing elit. Distinctio iure numquam modi voluptatem nam. Reprehenderit eos, aperiam optio dolorem eligendi nobis molestiae magnam placeat.'},
+  {name:'pic5.jpg', title:'Frieza', story:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure numquam modi voluptatem nam. Reprehenderit eos, aperiam optio dolorem eligendi nobis molestiae magnam placeat sit, architecto mollitia, ullam accusantium consequuntur'},
+  {name:'pic6.jpg', title:'SSJ2 Gohan', story:'Distinctio iure numquam modi voluptatem nam. Reprehenderit eos, aperiam optio dolorem eligendi nobis molestiae magnam placeat sit, architecto mollitia, ullam accusantium consequuntur.'},
+  {name:'pic7.jpg', title:'Gohan vs Cell', story:'Ipsum dolor sit amet consectetur adipisicing elit. Distinctio iure numquam modi voluptatem nam ullam accusantium consequuntur.'},
+  {name:'pic8.jpg', title:'Ultimate Gohan', story:'Adipisicing elit. Distinctio iure numquam modi voluptatem nam. Reprehenderit eos, aperiam optio dolorem eligendi nobis molestiae magnam placeat.'},
+];
+
+let currentPictureIndex = 0;
