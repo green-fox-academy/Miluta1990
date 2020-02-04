7c66084f2d450a1c3fc2fd6d257dc6eac77568a5
Milan Bartha <bartha.milan@gmail.hu>
2020-02-04 13:43:32 +0100

    finished one time clicker

--- week-08/day-02/count.js
+++ week-08/day-02/count.js
@@ -5,7 +5,7 @@ Count the items in the list
 And display the result in the result element.*/
 
 let button = document.querySelector('button');
-button.addEventListener('click', countElements());
+button.addEventListener('click', countElements);
 
 function countElements() {
     let result = document.getElementsByTagName('li').length;
---
+++ week-08/day-02/one-time-clicker.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/*Create an event listener which logs the current timestamp to the console once the button is clicked.
+      When the button is clicked for the second or any other times no event listener should be called.
+      Try to solve this two different ways.
+      hint: take a look at the button attributes https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button*/
+
+      let button = document.querySelector('button');
+      button.addEventListener('click', timeShow);
+
+      function timeShow (event) {
+          console.log(event.timeStamp);
+          button.removeEventListener('click', timeShow);
+      };
+
+  
+
+
+
