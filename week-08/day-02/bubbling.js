4cffbbdccfd2d15f6ef4324047b835cc2d70ab73
Milan Bartha <bartha.milan@gmail.hu>
2020-02-04 15:32:32 +0100

    bubbling finished working perfectly

--- week-08/day-02/bubbling.js
+++ week-08/day-02/bubbling.js
@@ -8,3 +8,39 @@
          - one for navigation
          - one for zooming*/
 
+let posX = 0;
+let posY = 0;
+let size = 1000;
+
+const navigation = (event) => {
+
+    let direction = event.target.dataset.direction;
+    let inspector = document.querySelector('.img-inspector');
+
+    if (direction === 'up') {
+        posY += 10;
+        inspector.style.backgroundPositionY = posY + 'px';
+    };
+    if (direction === 'down') {
+        posY -= 10;
+        inspector.style.backgroundPositionY = posY + 'px';
+    };
+    if (direction === 'left') {
+        posX -= 10;
+        inspector.style.backgroundPositionX = posX + 'px';
+    };
+    if (direction === 'right') {
+        posX += 10;
+        inspector.style.backgroundPositionX = posX + 'px';
+    };
+    if (direction === 'in') {
+        size += 20;
+        inspector.style.backgroundSize = size + 'px';
+    };
+    if (direction === 'out') {
+        size -= 20;
+        inspector.style.backgroundSize = size + 'px';
+    };
+};
+
+document.querySelector('nav').addEventListener('click', navigation);
