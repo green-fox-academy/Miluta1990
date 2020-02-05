3b669a4ae18ec039aef10086afca2be948b616dc
Milan Bartha <bartha.milan@gmail.hu>
2020-02-05 14:51:31 +0100

    working on style

---
+++ week-08/day-03/loginform/server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded());
+
+app.get('/' ,(req, res) => {
+    res.sendfile('./public/login_form.html');
+  });
+  
+  app.post('/send', (req, res) => {
+    console.log(req.body.user_name);
+    console.log(req.body.password);
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Listening on port number ${PORT}`);
+  });
