4e08f73d0cb48d1cdea5616051b5a69221b5d6f6
Milan Bartha <bartha.milan@gmail.hu>
2020-02-18 16:47:26 +0100

    test ok

---
+++ week-09/day-02/iamgroot/routes.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/groot', (req, res) => {
+    if (req.query.message) {
+        res.status(200).json({
+            "recieved": req.query.message,
+            "translated": "I am Groot!"
+        });
+    } else {
+        res.status(400).json({
+            "error": "I am Groot!"
+        });
+    };
+});
+
+module.exports = app;
