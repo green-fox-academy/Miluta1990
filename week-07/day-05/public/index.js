6320101c94d69ee120e54afbc6699f11a42201cf
Milan Bartha <bartha.milan@gmail.hu>
2020-02-21 10:24:48 +0100

    blabla

--- week-07/day-05/public/index.js
+++ week-07/day-05/public/index.js
@@ -5,3 +5,10 @@ const mainContent = document.querySelector('main');
 fetch('/posts')
   .then(res => res.text())
   .then(content => mainContent.innerText = content);
+
+let button = document.getElementsByClassName('submit');
+
+button.addEventListener('click', function(){
+    alert('only 1 button clicked');
+    window.location = 'http://www.google.hu';
+});
