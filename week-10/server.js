2e185899b49b64a4dde173945c3a1479afd2824e
Milan Bartha <bartha.milan@gmail.hu>
2020-02-24 17:20:03 +0100

    end day commit

--- week-10/server.js
+++ week-10/server.js
@@ -4,10 +4,14 @@ const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
 require('dotenv').config();
+const fs = require('fs');
 app.use(express.static('public'));
 app.use(express.json());
+app.use(bodyParser.json());
+
+let cors = require('cors');
 
 
 // DB connection
@@ -16,15 +20,46 @@ const connection =  mysql.createConnection({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     password: process.env.DB_PASS,
-    database: 'songs',
+    database: 'music',
 });
 
 const songsTable = 'songs';
-
+const playlistsTable = 'playlists'
 
 connection.connect((err) => {
-    err ? console.error(new Error(err)) : console.log(`Database *${songsTable}* connected!`);
+    err ? console.error(new Error(err)) : console.log(`Database *music* connected!`);
   });
+
+  // *** START OF ENDPOINTS ***
+
+// serving main page
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+  });
+
+// data getting endpoints
+//get playlist
+app.get('/playlists', function(req, res) {
+  let sql = "SELECT * FROM playlists";
+  connection.query(sql, function(err, result) {
+    if (err) {
+      res.status(500).send({ error: 'Something failed!' });
+    } else {
+      res.json(result);
+    }
+  })
+});
+
+app.get('/tracks', function(req, res) {
+  let sql = "select path from tracks inner join playlists on tracks.playlist_id = playlists.id where playlist_id = 1;";
+  connection.query(sql, function(err, result) {
+    if (err) {
+      res.status(500).send({ error: 'Something failed!' });
+    } else {
+      res.json(result);
+    }
+  })
+});
 
 
 app.listen(PORT, () => {
