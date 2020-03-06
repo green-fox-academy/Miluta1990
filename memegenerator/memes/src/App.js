5b50369a9d21cf2fd9d75b73438c96918e906d73
Milan Bartha <bartha.milan@gmail.hu>
2020-03-06 09:24:10 +0100

    memegenerator done

---
+++ memegenerator/memes/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MemeGenerator from './MemeGenerator';
+import Header from './header';
+
+function App() {
+  return (
+      <div>
+        <Header />
+        <MemeGenerator />
+      </div>
+  );
+}
+
+export default App;
